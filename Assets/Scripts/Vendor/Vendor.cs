afa461cd6c3377aed957696b0294367492069397
Julie Nginn <nginn.julie@gmail.com>
2020-08-13 19:43:41 +0200

    Feat: Add vendor sprite

---
+++ Assets/Scripts/Vendor/Vendor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vendor : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+        Debug.Log("Interact");
+    }
+
+    public void StopInteract()
+    {
+        Debug.Log("StopInteract");
+    }
+}
