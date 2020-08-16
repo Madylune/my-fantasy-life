1a61d04ea60d7ffe21a652e5ba2cfe3fd24b81c4
Julie Nginn <nginn.julie@gmail.com>
2020-08-16 15:43:18 +0200

    Feat: Add QuestGiver quesrs

--- Assets/Scripts/Quest/QuestGiverPanel.cs
+++ Assets/Scripts/Quest/QuestGiverPanel.cs
@@ -5,7 +5,24 @@ using UnityEngine.UI;
 
 public class QuestGiverPanel : Panel
 {
+    private static QuestGiverPanel instance;
+
+    public static QuestGiverPanel MyInstance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<QuestGiverPanel>();
+            }
+            return instance;
+        }
+    }
+
     private QuestGiver questGiver;
+
+    [SerializeField]
+    private GameObject actionButtons, questDescription;
 
     [SerializeField]
     private GameObject questPrefab;
@@ -21,6 +38,8 @@ public class QuestGiverPanel : Panel
         {
             GameObject go = Instantiate(questPrefab, questArea);
             go.GetComponent<Text>().text = quest.MyTitle;
+
+            go.GetComponent<QuestGiverQuestScript>().MyQuest = quest;
         }
     }
 
@@ -28,5 +47,23 @@ public class QuestGiverPanel : Panel
     {
         ShowQuests((npc as QuestGiver));
         base.Open(npc);
+    }
+
+    public void ShowQuestInfo(Quest quest)
+    {
+        actionButtons.SetActive(true);
+        questArea.gameObject.SetActive(false);
+        questDescription.SetActive(true);
+
+        string title = quest.MyTitle;
+        string description = quest.MyDescription;
+        string objectives = string.Empty;
+
+        foreach (Objective obj in quest.MyCollectObjectives)
+        {
+            objectives = obj.MyType + ": " + obj.MyCurrentAmount + "/" + obj.MyAmount + "\n";
+        }
+
+        questDescription.GetComponent<Text>().text = string.Format("<b>{0}</b>\n\n<size=13>{1}</size>\n\n<size=12><color=red>{2}</color></size>", "[ " + title + " ]", description, objectives);
     }
 }
---
+++ Assets/Scripts/Quest/QuestGiverQuestScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestGiverQuestScript : MonoBehaviour
+{
+    public Quest MyQuest { get; set; }
+
+    public void Select()
+    {
+        QuestGiverPanel.MyInstance.ShowQuestInfo(MyQuest);
+    }
+}
