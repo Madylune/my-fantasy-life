c4b69a445ca117de1c6003dba6f0890d1cf1030d
Julie Nginn <nginn.julie@gmail.com>
2020-08-31 18:04:06 +0200

    Feat: Character selection UI

---
+++ Assets/Scripts/Menu/CharacterSelection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelection : MonoBehaviour
+{
+    [SerializeField]
+    private Button[] charSlots;
+
+    public void SelectCharacter(int index)
+    {
+        // Load character data
+    }
+}
